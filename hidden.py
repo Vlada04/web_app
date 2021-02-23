026a47c9871760083f74a02f57acda93ccb10f32
Vlada04 <73779916+Vlada04@users.noreply.github.com>
2021-02-23 21:10:16 +0200

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,14 @@
+def oauth():
+    return {"consumer_key": "8vI1Y5OgU4yaXwHY4FeCYRfEM",
+            "consumer_secret": "OY3RbMQ97hlvtEcrm5soHT1dnJgWv2bspS8xrb3F3AgvfqbWH3",
+            "token_key": "1362142277968293889-U0xgA9RSBb4cPiogUPHqJgT6uypv1g",
+            "token_secret": "hjHPIneBTfHrP5rNZMXY84yhVuA05xSuURV6mCF29q9pH"}
+
+# API key
+# 8vI1Y5OgU4yaXwHY4FeCYRfEM
+
+# API secret key
+# OY3RbMQ97hlvtEcrm5soHT1dnJgWv2bspS8xrb3F3AgvfqbWH3
+
+# Bearer token
+# AAAAAAAAAAAAAAAAAAAAACD3MwEAAAAAgrfSdC6W5V60DoD3suONscfMnDg%3Dkr2c2s4uvimX1RIKpn1wCYCjT4MKT84CJKonQCFHFeYIuCF8nT
